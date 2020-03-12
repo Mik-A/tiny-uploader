645e17071c67ed177058545ce98a0efe13392889
Mik-A <mika.valimaki@gmail.com>
2020-03-12 08:58:27 +0200

    patch props check

--- src/index.js
+++ src/index.js
@@ -1,53 +1,42 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
-const UploadTextFile = (props) => {
+const UploadTextFile = props => {
   const { fileContentHandler, fileInfo, label, className } = props;
-  const suppress = (e) => {
-     e.stopPropagation()
-     e.preventDefault()
-   }
-   const onDrop = async (e) => {
-     e.stopPropagation()
-     e.preventDefault()
-     const file = e.dataTransfer.files[0]
-     let text = await file.text()
-     if (file) {
-       fileInfo(file)
-       fileContentHandler(text)
-     }
-   }
-   const onChangeHandler = async (e) => {
-     const file = e.target.files[0]
-     let text = await file.text()
+  const suppress = e => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+  const onDrop = async e => {
+    e.stopPropagation();
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    let text = await file.text();
+    if (fileContentHandler != null) fileContentHandler(text);
+    if (fileInfo != null) fileInfo(file);
+  };
+  const onChangeHandler = async e => {
+    const file = e.target.files[0];
+    let text = await file.text();
 
-     if(fileContentHandler != null) {
-     fileContentHandler(text)
-     }
-     if(fileInfo) {
-     fileInfo(file)
-     }
-   }
-   return (
-     <div
-       onDrop={onDrop}
-       onDragEnter={suppress}
-       onDragOver={suppress}
-     >
-       <form method='post' action='#' id='#'>
-           <label className={`tiny-uploader ${className ? className : ''}`}>
-             {label ? label : 'Upload text'}
-             <input
-               type='file'
-               name='file'
-               onChange={onChangeHandler}
-               style={{ display: 'none' }}
-             />
-           </label>          
-       </form>
-     </div>
-   )
- }
- 
- export default UploadTextFile
- 
+    if (fileContentHandler != null) fileContentHandler(text);
+    if (fileInfo != null) fileInfo(file);
+  };
+  return (
+    <div onDrop={onDrop} onDragEnter={suppress} onDragOver={suppress}>
+      <form method="post" action="#" id="#">
+        <label className={`tiny-uploader ${className ? className : ""}`}>
+          {label ? label : "Upload text"}
+          <input
+            type="file"
+            name="file"
+            onChange={onChangeHandler}
+            style={{ display: "none" }}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default UploadTextFile;
