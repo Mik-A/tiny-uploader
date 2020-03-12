9164b22b84f61360a6b840e353e83a4a1d901802
Mik-A <mika.valimaki@gmail.com>
2020-03-12 08:54:01 +0200

    update props and readme.md

--- readme.md
+++ readme.md
@@ -1,2 +1,58 @@
 # tiny-uploader
 
+Customizable React component/button for uploading text files. 
+
+Works with most common text documents such as .txt and .md
+
+## Installation
+
+`npm i @mik-a/tiny-uploader --save`
+
+## Usage
+
+```jsx
+import UploadeTextFile from '@mik-a/tiny-uploader'
+
+<UploadTextFile fileUploadHandler={fileUploadHandler} fileInfo={fileInfoHandler} label='Upload' className='my-custom-class' />
+
+// Example of functions
+
+  const fileUploadHandler = e => {
+    console.log('text content: ', e)
+  }
+
+  const fileInfoHandler = e => {
+    console.log('file info', e)
+    // name, lastModified, webkitRelativePath , size , type 
+  }
+
+```
+## Props
+
+`fileUploadHandler` // function()
+
+`FileInfo` // function()
+
+### Optional props
+
+`label` // 'string' - default 'Upload text'
+
+`className` // 'string' - your custom CSS class
+
+## Default CSS
+```css
+.tiny-uploader {
+	border: 1px solid;
+	padding: 0.8rem 1.8rem;
+	border-radius: 1.8rem;
+}
+
+```
+
+## Author
+- **Mika Välimäki** - [GitHub profile](https://github.com/Mik-A)
+- **Phone** - [+358 50 4088 201](+358504088201)
+- **Mika Välimäki** - [LinkedIn](https://www.linkedin.com/in/mika-v%C3%A4lim%C3%A4ki/)
+
+- **GitHub repo** - [tiny-uploader](https://github.com/Mik-A/tiny-uploader)
+
--- src/index.js
+++ src/index.js
@@ -1,55 +1,53 @@
 import React from 'react'
-/*
-  Simple HTML5 file drag-and-drop wrapper
-  usage: <UploadTextFile handleFile={handleFile}>...</UploadTextFile>
-    handleFile(file:File):void;
-*/
+import './styles.css'
+
 const UploadTextFile = (props) => {
- const { fileUploadHandler, updateFileName } = props;
+  const { fileContentHandler, fileInfo, label, className } = props;
+  const suppress = (e) => {
+     e.stopPropagation()
+     e.preventDefault()
+   }
+   const onDrop = async (e) => {
+     e.stopPropagation()
+     e.preventDefault()
+     const file = e.dataTransfer.files[0]
+     let text = await file.text()
+     if (file) {
+       fileInfo(file)
+       fileContentHandler(text)
+     }
+   }
+   const onChangeHandler = async (e) => {
+     const file = e.target.files[0]
+     let text = await file.text()
 
-  const suppress = (evt) => {
-    evt.stopPropagation()
-    evt.preventDefault()
-  }
-  const onDrop = (evt) => {
-    evt.stopPropagation()
-    evt.preventDefault()
-    const files = evt.dataTransfer.files
-    if (files && files[0]) props.handleFile(files[0])
-  }
-  const onChangeHandler = async (event) => {
-    var file = event.target.files[0]
-    let text = await file.text()
-    if(fileUploadHandler) {
-    fileUploadHandler(text)
-    }
-    if(updateFileName) {
-    updateFileName(file)
-    }
-  }
-  return (
-    <div
-      className=''
-      onDrop={onDrop}
-      onDragEnter={suppress}
-      onDragOver={suppress}
-    >
-      <form method='post' action='#' id='#'>
-        <div className='grid-2-col'>
-          {props.fileName}
-          <label className='file-handler'>
-            Upload Your File
-            <input
-              type='file'
-              name='file'
-              onChange={onChangeHandler}
-              style={{ display: 'none' }}
-            />
-          </label>
-        </div>
-      </form>
-    </div>
-  )
-}
-
-export default UploadTextFile
+     if(fileContentHandler != null) {
+     fileContentHandler(text)
+     }
+     if(fileInfo) {
+     fileInfo(file)
+     }
+   }
+   return (
+     <div
+       onDrop={onDrop}
+       onDragEnter={suppress}
+       onDragOver={suppress}
+     >
+       <form method='post' action='#' id='#'>
+           <label className={`tiny-uploader ${className ? className : ''}`}>
+             {label ? label : 'Upload text'}
+             <input
+               type='file'
+               name='file'
+               onChange={onChangeHandler}
+               style={{ display: 'none' }}
+             />
+           </label>          
+       </form>
+     </div>
+   )
+ }
+ 
+ export default UploadTextFile
+ 
